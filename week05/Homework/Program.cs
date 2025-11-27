cb61748428590ba4131c3b337a2c8e1ff917de79
Adriano-Mori <amorihuamani@byupathway.edu>
2025-11-27 17:42:08 -0500

    finished the week05 inheritance activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // main assignment
+        Assignment a1 = new Assignment("Alex Mori", "Integrals");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine("");
+        //derived
+        MathAssignment a2 = new MathAssignment("Laura Silva", "Algebra", "5.2", "10-24");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment a3 = new WritingAssignment("John Doe", "Modern Literature", "Symbolism in 20th Century Poetry");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+        Console.WriteLine("");
     }
 }
